adf42b5dfdef028da3ea4b435936d99fe81c52df
naeemtaj <naheemtaj44@gmail.com>
2024-07-19 18:11:35 +0500

    add

--- python/firstprogram.py
+++ python/firstprogram.py
@@ -1,37 +1,129 @@
-"""print("hello world")
-print("i am naheem taj")
-print("im 19 year old.")
-print("naheem taj","balochistAN")
-print(2342)
-print("baloch")
-print(12+12*2)
-name="naeemtaj"
-age=23
-price=23.23
-print(name)
-print(age)
-print(price)
-a=12
-b=3
-print(a+b)
-print("my name is",name)
-print("my age is",age)
-print(23)
-age2=age
-print=(age2)
+# print("hello world")
+# print("i am naheem taj")
+# print("im 19 year old.")
+# print("naheem taj","balochistAN")
+# print(2342)
+# print("baloch")
+# print(12+12*2)
+# name="naeemtaj"
+# age=23
+# price=23.23
+# print(name)
+# print(age)
+# print(price)
+# a=12
+# b=3
+# print(a+b)
+# print("my name is",name)
+# print("my age is",age)
+# print(23)
+# age2=age
+# print=(age2)
 
-print(type(age2))
-print(type(price))
-print(type(name))"""
-name1='naeem'
-name2="taj"
-name3='''pk'''
-print(name1)
-print(name2)
-print(name3)
-print(type(name1))
-a2=False
-c=12
-n=32
-sum=c+n
-print(sum)
+# print(type(age2))
+# print(type(price))
+# print(type(name))"""
+# name1='naeem'
+# name2="taj"
+# name3='''pk'''
+# print(name1)
+# print(name2)
+# print(name3)
+# print(type(name1))
+# a2=False
+# c=12
+# n=32
+# sum=c+n
+# print(sum)
+
+#arithmetic operation
+# a=4
+# b=4
+# print(a-b)
+# print(a+b)
+# print(a*b)
+# print(a/b)
+# print(a%b) #remaider/mudeli
+# print(a**b)#power operator
+#relational operator
+# a=12
+# b=33
+# print(a==b)#false
+# print(a!=b)#tlue
+# print(a<=b)#true
+# print(a<b)#true
+# print(a>b#false)
+
+
+# #assigment operator
+# num=10
+# num=num+10 #10+10=20
+# print("num:",num)
+
+#assigment operator
+# num=10
+# num*=10
+# print("num:",num)# num*1o=100
+
+# #assigment operator
+# num=10
+# num**=10
+# print("num:",num)# num*1o=1000000
+
+
+#logical operator
+# a=30
+# b=15
+# print(not False)
+# print(not True)
+
+
+# #logical operator
+# a=30
+# b=15
+# print(not False)
+# print(not (a>b))
+
+# val1=True
+# val2=True
+# print("And operator:",val1 and val2)
+
+# val1=False
+# val2=False
+# print("And operator:",val1 and val2)
+
+# val1=False
+# val2=True
+# print("And operator:",val1 and val2)
+
+
+# val1=True
+# val2=False
+# print("Or operator:",val1 or val2)
+
+# #type conversion
+# a=2
+# b=2.25
+# sum=a+b
+# print("sum:",sum)# 2+2.25=4.25
+
+# a= int("2")
+# b=2.25
+# sum= a+b
+# print(type(a))
+# print("sum:",sum)
+
+# input("enter your name:")
+
+# name=input("enter your name")
+# print("welcome:",name)
+
+# val=input("enter your age")
+# print(type(val),val)
+
+val=int(input("enter your age"))
+print(type(val),val)
+
+
+
+
