951bbe5bd55771d7292e484b6115b875b8a28543
naeemtaj <naheemtaj44@gmail.com>
2024-07-19 08:44:37 +0500

    add task

---
+++ python/firstprogram.py
@@ -0,0 +1,37 @@
+"""print("hello world")
+print("i am naheem taj")
+print("im 19 year old.")
+print("naheem taj","balochistAN")
+print(2342)
+print("baloch")
+print(12+12*2)
+name="naeemtaj"
+age=23
+price=23.23
+print(name)
+print(age)
+print(price)
+a=12
+b=3
+print(a+b)
+print("my name is",name)
+print("my age is",age)
+print(23)
+age2=age
+print=(age2)
+
+print(type(age2))
+print(type(price))
+print(type(name))"""
+name1='naeem'
+name2="taj"
+name3='''pk'''
+print(name1)
+print(name2)
+print(name3)
+print(type(name1))
+a2=False
+c=12
+n=32
+sum=c+n
+print(sum)
